cc20f76adb8400fbf128324736a1e00573743547
claudiumihait <claudium.tudor@gmail.com>
2023-02-21 09:18:30 +0200

    Implement MovementPhase

---
+++ src/main/java/com/codecool/marsexploration/logic/phase/MovementPhase.java
@@ -0,0 +1,13 @@
+package com.codecool.marsexploration.logic.phase;
+
+import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.Rover;
+
+public class MovementPhase implements Phase{
+
+    @Override
+    public void perform(Context context) {
+        Rover rover = context.rover();
+        rover.state().move(context);
+    }
+}
