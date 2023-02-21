20f5b0ddd4ee45e862317281283d2b1f4cf78de0
claudiumihait <claudium.tudor@gmail.com>
2023-02-21 12:28:13 +0200

    Implement Log Phase

--- src/main/java/com/codecool/marsexploration/logic/phase/LogPhase.java
+++ src/main/java/com/codecool/marsexploration/logic/phase/LogPhase.java
@@ -1,11 +1,18 @@
 package com.codecool.marsexploration.logic.phase;
 
 import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.utils.LogSaver;
 
 public class LogPhase implements Phase{
 
+    private final LogSaver writer;
+
+    public LogPhase(LogSaver writer) {
+        this.writer = writer;
+    }
+
     @Override
     public void perform(Context context) {
-
+            writer.logStep(context);
     }
 }
