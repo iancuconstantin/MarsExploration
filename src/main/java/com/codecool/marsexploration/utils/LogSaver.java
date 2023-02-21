64fb41a0b61d691aa11d56610bd0dae8315c5a00
claudiumihait <claudium.tudor@gmail.com>
2023-02-21 21:38:43 +0200

    Fixed syntax for appending log entries to file

--- src/main/java/com/codecool/marsexploration/utils/LogSaver.java
+++ src/main/java/com/codecool/marsexploration/utils/LogSaver.java
@@ -11,8 +11,9 @@ public class LogSaver {
     public void logStep(Context context){
         //STEP 6; EVENT position; UNIT rover-1; POSITION [21,22]
         String content = "STEP-" + context.getStepNumber() + "; EVENT position; UNIT ROVER-" + context.getRover().getId() + "; POSITION [" + context.getRover().getCoordinate().x() + "," +context.getRover().getCoordinate().y() + "]";
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(context.getLogPath()))){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(context.getLogPath(), true))){
             bw.append(content);
+            bw.newLine();
         }catch(IOException e){
             System.out.println("An error occurred while trying to write the file.\n Error message: " + e.getMessage());
         }
