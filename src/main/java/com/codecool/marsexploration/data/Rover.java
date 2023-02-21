bf37a2b0bb531b82fed083f090ec8ace3d0915c8
claudiumihait <claudium.tudor@gmail.com>
2023-02-21 12:25:59 +0200

    Convert Rover from record to class

--- src/main/java/com/codecool/marsexploration/data/Rover.java
+++ src/main/java/com/codecool/marsexploration/data/Rover.java
@@ -5,10 +5,70 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public record Rover(
-        int id,
-        Coordinate coordinate,
-        int sight,
-        Routine state,
-        List<Coordinate> trackRecord,
-        Map<Coordinate, String> sightings) {}
+public class Rover {
+    private int id;
+    private Coordinate coordinate;
+    private int sight;
+    private Routine state;
+    private List<Coordinate> trackRecord;
+    private Map<Coordinate, String> sightings;
+
+    public Rover(int id, Coordinate coordinate, int sight, Routine state,
+                 List<Coordinate> trackRecord, Map<Coordinate, String> sightings) {
+        this.id = id;
+        this.coordinate = coordinate;
+        this.sight = sight;
+        this.state = state;
+        this.trackRecord = trackRecord;
+        this.sightings = sightings;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public int getSight() {
+        return sight;
+    }
+
+    public void setSight(int sight) {
+        this.sight = sight;
+    }
+
+    public Routine getState() {
+        return state;
+    }
+
+    public void setState(Routine state) {
+        this.state = state;
+    }
+
+    public List<Coordinate> getTrackRecord() {
+        return trackRecord;
+    }
+
+    public void setTrackRecord(List<Coordinate> trackRecord) {
+        this.trackRecord = trackRecord;
+    }
+
+    public Map<Coordinate, String> getSightings() {
+        return sightings;
+    }
+
+    public void setSightings(Map<Coordinate, String> sightings) {
+        this.sightings = sightings;
+    }
+
+}
