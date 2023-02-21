641abdb4d9c75d02da20c687b65701482524aa12
claudiumihait <claudium.tudor@gmail.com>
2023-02-21 13:13:44 +0200

    Implement file reader class

---
+++ src/main/java/com/codecool/marsexploration/utils/ReadFile.java
@@ -0,0 +1,21 @@
+package com.codecool.marsexploration.utils;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ReadFile {
+
+    public String readFile(String filePath){
+        StringBuilder sb = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String line;
+            while((line = br.readLine()) != null)
+                sb.append(line).append("\n");
+        }catch(IOException e){
+            System.out.println("An error occurred while reading the file.");
+        }
+        return sb.toString();
+    }
+}
