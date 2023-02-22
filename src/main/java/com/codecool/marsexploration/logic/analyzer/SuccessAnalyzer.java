c1a3f6bb55cf18fac04ad5ffaa677319a93414a6
claudiumihait <claudium.tudor@gmail.com>
2023-02-22 12:34:28 +0200

    Implement SuccessAnalyzer

--- src/main/java/com/codecool/marsexploration/logic/analyzer/SuccessAnalyzer.java
+++ src/main/java/com/codecool/marsexploration/logic/analyzer/SuccessAnalyzer.java
@@ -1,15 +1,32 @@
 package com.codecool.marsexploration.logic.analyzer;
 
 import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.Outcome;
+import com.codecool.marsexploration.data.Symbol;
 
+import java.util.Map;
 import java.util.Optional;
 
 public class SuccessAnalyzer implements Analyzer{
 
+    private final int RESOURCES_REQUIRED_FOR_COLONIZATION = 10;
+
     @Override
     public Optional<Outcome> analyze(Context context) {
-        context.getRover().getSightings();
+        Map<Coordinate, String> sightings = context.getRover().getSightings();
+        int mineralsCount = 0;
+        int waterCount = 0;
+
+        for (Map.Entry<Coordinate, String> entry: sightings.entrySet()){
+            if (entry.getValue().equals(Symbol.MINERAL.getSymbol()))
+                mineralsCount++;
+            if (entry.getValue().equals(Symbol.WATER.getSymbol()))
+                waterCount++;
+
+            if(mineralsCount + waterCount == RESOURCES_REQUIRED_FOR_COLONIZATION)
+                return Optional.of(Outcome.COLONIZABLE);
+        }
         return Optional.empty();
     }
 }
