09edf82ceeb5c1f63bb7429bb5d3eb99e63ac26b
claudiumihait <claudium.tudor@gmail.com>
2023-02-23 12:30:25 +0200

    Refactor LackOfResourcesAnalyzer logic

--- src/main/java/com/codecool/marsexploration/logic/analyzer/LackOfResourcesAnalyzer.java
+++ src/main/java/com/codecool/marsexploration/logic/analyzer/LackOfResourcesAnalyzer.java
@@ -12,14 +12,16 @@ public class LackOfResourcesAnalyzer implements Analyzer {
 
     private static final int MINERAL_THRESHOLD = 1;
     private static final int WATER_THRESHOLD = 1;
-    private static final int MAX_ITERATIONS_WITHOUT_RESOURCES = 50;
+    private int MAX_ITERATIONS_WITHOUT_RESOURCES;
+    private int iterationsWithoutResources;
 
     @Override
     public Optional<Outcome> analyze(Context context) {
+        if(iterationsWithoutResources == 0) setIterationsLimit(context);
+
         Map<Coordinate, String> sightings = context.getRover().getSightings();
         int mineralsCount = 0;
         int waterCount = 0;
-        int iterationsWithoutMinResources = 0;
 
         for (Map.Entry<Coordinate, String> entry : sightings.entrySet()) {
             if (entry.getValue().equals(Symbol.MINERAL.getSymbol()))
@@ -27,17 +29,21 @@ public class LackOfResourcesAnalyzer implements Analyzer {
             else if (entry.getValue().equals(Symbol.WATER.getSymbol()))
                 waterCount++;
 
-            if (mineralsCount >= MINERAL_THRESHOLD || waterCount >= WATER_THRESHOLD){
-                iterationsWithoutMinResources = 0;
-            }else{
-                iterationsWithoutMinResources++;
-            }
-
-            if (iterationsWithoutMinResources >= MAX_ITERATIONS_WITHOUT_RESOURCES){
-                return Optional.of(Outcome.NOT_COLONIZABLE_RESOURCES);
+            if (mineralsCount >= MINERAL_THRESHOLD && waterCount >= WATER_THRESHOLD){
+                iterationsWithoutResources = 0;
             }
         }
 
+        iterationsWithoutResources++;
+
+        if (iterationsWithoutResources >= MAX_ITERATIONS_WITHOUT_RESOURCES){
+            return Optional.of(Outcome.NOT_COLONIZABLE_RESOURCES);
+        }
+
         return Optional.empty();
+    }
+
+    private void setIterationsLimit(Context context) {
+        MAX_ITERATIONS_WITHOUT_RESOURCES = (int) (context.getTimeout() / 3);
     }
 }
