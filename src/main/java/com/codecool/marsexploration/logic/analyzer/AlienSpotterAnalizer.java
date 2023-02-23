f1be0bb11f3df988727dab61df4390c2d1dae7d9
iancuconstantin <iancu.constantin95@gmail.com>
2023-02-23 11:07:29 +0200

    [Constantin] AlienSpoter Analizer created

--- src/main/java/com/codecool/marsexploration/logic/analyzer/AlienSpotterAnalizer.java
+++ src/main/java/com/codecool/marsexploration/logic/analyzer/AlienSpotterAnalizer.java
@@ -1,14 +1,25 @@
 package com.codecool.marsexploration.logic.analyzer;
 
 import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.Outcome;
+import com.codecool.marsexploration.data.Symbol;
 
+import java.util.Map;
 import java.util.Optional;
 
 public class AlienSpotterAnalizer implements Analyzer{
 
     @Override
     public Optional<Outcome> analyze(Context context) {
+
+        Map<Coordinate,String> sightings = context.getRover().getSightings();
+        for(Map.Entry<Coordinate,String> entry:sightings.entrySet()){
+            if(entry.getValue().equals(Symbol.ALIEN.getSymbol())){
+                return Optional.of(Outcome.NOT_COLONIZABLE_ALIENS);
+            }
+        }
+
         return Optional.empty();
     }
 }
