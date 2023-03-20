2aa4e4f994c246265f0cdedeafc6d5ea628b047d
claudiumt <claudium.tudor@gmail.com>
2023-03-20 03:23:04 +0200

    Refactor Log Saver with new helper class

---
+++ src/main/java/com/codecool/marsexploration/utils/LogMessageCreator.java
@@ -0,0 +1,70 @@
+package com.codecool.marsexploration.utils;
+
+import com.codecool.marsexploration.data.rover.Explorer;
+import com.codecool.marsexploration.data.rover.Gatherer;
+
+import java.util.UUID;
+
+import static com.codecool.marsexploration.data.Symbol.MINERAL;
+
+public class LogMessageCreator
+
+{
+	public static String createExplorerPositionLogMessage(int stepNumber, Explorer explorer){
+		return  "STEP-" + stepNumber +
+				"; EVENT position; UNIT EXPLORING ROVER-" + explorer.getId() +
+				"; POSITION [" + explorer.getCurrentLocation().x() + "," + explorer.getCurrentLocation().y() + "]";
+	}
+
+	public static  String createFoundOutcomeLogMessage(String message){
+		return "\nEVENT outcome; OUTCOME " + message;
+	}
+
+	public static String createExplorerMoveToBuildingSpotLogMessage(int stepNumber, Explorer explorer){
+		return "STEP-" + stepNumber +
+				"; EVENT position(move to building spot); UNIT EXPLORING ROVER-" + explorer.getId() +
+				"; POSITION [" + explorer.getCurrentLocation().x() + "," +explorer.getCurrentLocation().y() + "]";
+	}
+
+	public static String createExplorerBuildingLogMessage(int stepNumber, Explorer explorer){
+		return "STEP-" + stepNumber +
+				"; EVENT building; UNIT ROVER- " + explorer.getId() +  "; Building step - " +
+				explorer.getCurrentStepsInConstruction() + "/" +
+				explorer.getSTEPS_NEEDED_FOR_CONSTRUCTION() +
+				"; COMMAND CENTRE LOCATION - " + explorer.getBuildCommandCentreSpot() +
+				"; RESOURCES USED - " + (60 - explorer.getStoredResources().get(MINERAL)) +
+				"/" + 20 +
+				"; Progress: " + (((60 - explorer.getStoredResources().get(MINERAL)) * 100) / 20.0) +"%";
+	}
+
+	public static String createGatheringLogMessage(int stepNumber, Gatherer gatherer){
+
+		return "STEP-" + stepNumber +
+				"; EVENT gathering; UNIT GATHERING ROVER-" + gatherer.getId() +
+				"; RESOURCES GATHERED - TYPE: " + gatherer.getAssignedResource().getResourceType().getSymbol() +
+				" / AMOUNT - " + gatherer.getGATHERING_POWER() +
+				"; POSITION [" + gatherer.getCurrentLocation().x() + "," + gatherer.getCurrentLocation().y() + "]";
+
+	};
+
+	public static String createDeliveringResourceLogMessage(int stepNumber, Gatherer gatherer){
+		return  "STEP-" + stepNumber +
+				"; EVENT delivering; UNIT GATHERING ROVER-" + gatherer.getId() +
+				"; RESOURCES GATHERED - TYPE: " + gatherer.getAssignedResource().getResourceType().getSymbol() +
+				" / AMOUNT - " + gatherer.getGATHERING_POWER() +
+				"; POSITION [" +gatherer.getCurrentLocation().x() + "," +gatherer.getCurrentLocation().y() + "]";
+	}
+
+	public static String createGathererReturnToBaseLogMessage(int stepNumber, Gatherer gatherer){
+		return  "STEP-" + stepNumber +
+				"; EVENT position (returning home); UNIT GATHERING ROVER-" + gatherer.getId() +
+				"; POSITION [" + gatherer.getCurrentLocation().x() + "," + gatherer.getCurrentLocation().y() + "]";
+
+	}
+
+	public static String createMoveToGatheringSpotLogMessage(int stepNumber, Gatherer gatherer){
+		return "STEP-" + stepNumber +
+				"; EVENT position (move to gathering spot); UNIT GATHERING ROVER-" + gatherer.getId() +
+				"; POSITION [" + gatherer.getCurrentLocation().x() + "," + gatherer.getCurrentLocation().y() + "]";
+	}
+}
--- src/main/java/com/codecool/marsexploration/utils/LogSaver.java
+++ src/main/java/com/codecool/marsexploration/utils/LogSaver.java
@@ -1,13 +1,12 @@
 package com.codecool.marsexploration.utils;
 
 import com.codecool.marsexploration.data.Context;
+import com.codecool.marsexploration.data.rover.Explorer;
 import com.codecool.marsexploration.data.rover.Gatherer;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import static com.codecool.marsexploration.data.Symbol.MINERAL;
+import static com.codecool.marsexploration.utils.LogMessageCreator.*;
 
 public class LogSaver {
 
@@ -15,56 +14,51 @@ public class LogSaver {
 
     public void logStep(Context context){
         String content ="";
+        Explorer explorer = context.getExplorer();
         if(context.noCommandCentreAvailable()){
-            if(!context.getExplorer().isOnBuildingRoutine()){
-                content = "STEP-" + context.getStepNumber() +
-                        "; EVENT position; UNIT EXPLORING ROVER-" + context.getExplorer().getId() +
-                        "; POSITION [" + context.getExplorer().getCurrentLocation().x() + "," + context.getExplorer().getCurrentLocation().y() + "]";
+
+            if(!explorer.isOnBuildingRoutine()){
+                content = createExplorerPositionLogMessage( context.getStepNumber(),explorer);
+
                 if (context.getOutcome().isPresent()) {
-                    content += "\nEVENT outcome; OUTCOME " + context.getOutcome().get().getStatusMessage();
+                    content += createFoundOutcomeLogMessage(context.getOutcome().get().getStatusMessage());
                 }
             } else {
-                if (!context.getExplorer().isBuilding()) {
-                    content = "STEP-" + context.getStepNumber() +
-                            "; EVENT position(move to building spot); UNIT EXPLORING ROVER-" + context.getExplorer().getId() +
-                            "; POSITION [" + context.getExplorer().getCurrentLocation().x() + "," +context.getExplorer().getCurrentLocation().y() + "]";
-                }else {
-                    content = "STEP-" + context.getStepNumber() +
-                            "; EVENT building; UNIT ROVER- " + context.getExplorer().getId() +  "; Building step - " +
-                            context.getExplorer().getCurrentStepsInConstruction() + "/" +
-                            context.getExplorer().getSTEPS_NEEDED_FOR_CONSTRUCTION() +
-                            "; COMMAND CENTRE LOCATION - " + context.getExplorer().getBuildCommandCentreSpot() +
-                            "; RESOURCES USED - " + (60 - context.getExplorer().getStoredResources().get(MINERAL)) +
-                            "/" + 20 +
-                            "; Progress: " + (((60 - context.getExplorer().getStoredResources().get(MINERAL)) * 100) / 20.0) +"%";
+                if (!explorer.isBuilding()) {
+                    content = createExplorerMoveToBuildingSpotLogMessage(context.getStepNumber(), explorer);
+                } else {
+                    content = createExplorerBuildingLogMessage(context.getStepNumber(), explorer);
                 }
             }
         }else{
+            // TODO - ASK ADAM ?
+            //            for(Gatherer gatherer :context.getCommandCentres().get(0).getGatherers() ){
+            //                if (gatherer.isAtGatheringSpot()) {
+            //                    content = createGatheringLogMessage(context.getStepNumber(),  gatherer);
+            //                } else if(gatherer.isAtCommandCentreSpot() && gatherer.hasGathered()) {
+            //                    content = createDeliveringResourceLogMessage(context.getStepNumber(), gatherer);
+            //                } else if (gatherer.hasGathered() && !gatherer.isAtCommandCentreSpot()) {
+            //                    content = createGathererReturnToBaseLogMessage(context.getStepNumber(), gatherer);
+            //
+            //                } else{
+            //                    content = createMoveToGatheringSpotLogMessage(context.getStepNumber(),gatherer);
+            //                }
+            //                context.incrementStepNumber();
+            //            }
             Gatherer rover = context.getCommandCentres().get(0).getGatherers().get(gathererIndex);
             gathererIndex = gathererIndex + 1 > context.getCommandCentres().get(0).getGatherers().size() - 1
                     ? 0
                     : gathererIndex + 1;
+            System.out.println(gathererIndex);
 
             if (rover.isAtGatheringSpot()) {
-                content = "STEP-" + context.getStepNumber() +
-                        "; EVENT gathering; UNIT GATHERING ROVER-" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getId() +
-                        "; RESOURCES GATHERED - TYPE: " + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getAssignedResource().getResourceType().getSymbol() +
-                        " / AMOUNT - " + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getGATHERING_POWER() +
-                        "; POSITION [" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().x() + "," + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().y() + "]";
-            } else if(rover.isAtCommandCentreSpot() && rover.hasGathered()){
-                //deliver resources to command centre step
-                content = "STEP-" + context.getStepNumber() +
-                        "; EVENT delivering; UNIT GATHERING ROVER-" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getId() +
-                        "; RESOURCES DELIVERED - TYPE: " + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getGATHERING_POWER() +
-                        "; POSITION [" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().x() + "," + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().y() + "]";
-            }else if (rover.hasGathered() && !rover.isAtCommandCentreSpot()) {
-                content = "STEP-" + context.getStepNumber() +
-                        "; EVENT position (returning home); UNIT GATHERING ROVER-" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getId() +
-                        "; POSITION [" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().x() + "," + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().y() + "]";
-            }else{
-                content = "STEP-" + context.getStepNumber() +
-                        "; EVENT position (move to gathering spot); UNIT GATHERING ROVER-" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getId() +
-                        "; POSITION [" + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().x() + "," + context.getCommandCentres().get(0).getGatherers().get(gathererIndex).getCurrentLocation().y() + "]";;
+                content = createGatheringLogMessage(context.getStepNumber(),  rover);
+            } else if(rover.isAtCommandCentreSpot() && rover.hasGathered()) {
+                content = createDeliveringResourceLogMessage(context.getStepNumber(), rover);
+            } else if (rover.hasGathered() && !rover.isAtCommandCentreSpot()) {
+                    content = createGathererReturnToBaseLogMessage(context.getStepNumber(), rover);
+            } else{
+                content = createMoveToGatheringSpotLogMessage(context.getStepNumber(),rover);
             }
 
         }
